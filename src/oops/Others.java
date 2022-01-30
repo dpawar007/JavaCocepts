315eae7764bf2d10f7243f9f99359a224b7dafe2
Dipak Pawar <dbpawar877@gmail.com>
2022-01-30 19:43:17 +0530

    commit from personal laptop

---
+++ src/oops/Others.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Others {
+
+	public static void main(String[] args) {
+		
+		String[] argh={"A","B"};
+		args=argh;
+		for(String s : args) {
+		System.out.println(s);
+		}
+
+	}
+
+}
